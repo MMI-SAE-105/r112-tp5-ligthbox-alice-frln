3386fa8fa912478f53d6199d650008516466c129
alice-frln <alice.frelin@edu.univ-fcomte.fr>
2024-12-04 17:31:01 +0100

    correction d'affichage d'image

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,14 @@
 // *** LIGHTBOX ***
 
 const lightBox = document.querySelector("#lightbox") ;
-const lightboximg = document.querySelector("lightbox img") ;
+const lightboximg = document.querySelector("#lightbox img") ;
 
 document.addEventListener("click", function (e) {
     if (e.target.matches("[data-full-img]") || lightBox.open) {
       if (lightBox.open) {
         lightBox.close() ;
       } else {
+        lightboximg.src = e.target.dataset.fullImg;
         lightBox.showModal() ;
       }
     }
