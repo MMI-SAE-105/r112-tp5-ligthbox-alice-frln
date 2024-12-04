cfa92f50e13ad19c4083d0eeea1053e3e8114816
alice-frln <alice.frelin@edu.univ-fcomte.fr>
2024-12-04 17:24:46 +0100

    ajout évènement clic, vérification de l'ouverture de l'image, ouvertue avec showmodal

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,14 @@
 // *** LIGHTBOX ***
 
-const lightBox = document.querySelector("#lightbox");
-const lightboximg = document.querySelector("lightbox img");
+const lightBox = document.querySelector("#lightbox") ;
+const lightboximg = document.querySelector("lightbox img") ;
+
+document.addEventListener("click", function (e) {
+    if (e.target.matches("[data-full-img]") || lightBox.open) {
+      if (lightBox.open) {
+        lightBox.close() ;
+      } else {
+        lightBox.showModal() ;
+      }
+    }
+});
